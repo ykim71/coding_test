4166937368ffae2bf01951958e7c94f70f282562
Yujin Kim <yujin71@gmail.com>
2022-05-24 10:07:41 -0500

    complete

--- HackerRank/staircase.py
+++ HackerRank/staircase.py
@@ -1,2 +1,33 @@
-### Staircase ###
+#!/bin/python3
 
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'staircase' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+
+def staircase(n):
+    for i in range(1, n+1):
+        print(' '*(n-i)+'#'*i)
+        
+
+if __name__ == '__main__':
+#    n = int(input().strip())
+
+    staircase(9)
+
+
+''' expected outputs
+     #
+    ##
+   ###
+  ####
+ #####
+######
+'''
